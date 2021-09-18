a6a20c940ad8257de9cd1a53ea8b201e3b844e08
Luiz Felipe Montuani <luiz.silva16@estudante.ufla.br>
2021-09-17 22:43:57 -0300

    adiciona SingleChildScrollView homepage

--- lib/app/pages/continents_page/continents_page.dart
+++ lib/app/pages/continents_page/continents_page.dart
@@ -21,32 +21,35 @@ class _ContinentsPageState extends State<ContinentsPage> {
         centerTitle: true,
         backgroundColor: secondaryColor,
       ),
-      body: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20.5, horizontal: 14.5),
-          child: ListView.builder(
-            itemCount: widget.continents.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    child: Image.asset('assets/images/world.png'),
+      body: SingleChildScrollView(
+        child: Scaffold(
+          body: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.5, horizontal: 14.5),
+            child: ListView.builder(
+              itemCount: widget.continents.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Image.asset('assets/images/world.png'),
+                    ),
+                    title: Text(widget.continents[index].continent),
+                    subtitle: Text(
+                        '${widget.continents[index].countries.length} países'),
+                    trailing: Icon(Icons.arrow_forward),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Scaffold(),
+                        ),
+                      );
+                    },
                   ),
-                  title: Text(widget.continents[index].continent),
-                  subtitle: Text(
-                      '${widget.continents[index].countries.length} países'),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => Scaffold(),
-                      ),
-                    );
-                  },
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         ),
       ),
