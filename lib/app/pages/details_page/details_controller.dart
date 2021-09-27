c0770aaca581a88e3feef5984bfc163db70d2cf2
Luiz Montuani <luizmontuani@ioasys.com.br>
2021-09-26 22:18:36 -0300

    trying to implement add and remove from list

--- lib/app/pages/details_page/details_controller.dart
+++ lib/app/pages/details_page/details_controller.dart
@@ -1,9 +1,12 @@
+import 'package:covid_data/app/models/country.dart';
+import 'package:covid_data/app/pages/favorites_page/favorites_store.dart';
 import 'package:covid_data/app/repositories/country_repository.dart';
 import 'package:covid_data/app/pages/details_page/details_store.dart';
 
 class DetailsController {
   DetailsStore detailsStore;
   CountryRepository repository;
+  FavoritesStore store = FavoritesStore();
 
   DetailsController({required this.detailsStore, required this.repository});
 
@@ -12,5 +15,16 @@ class DetailsController {
     final country = await repository.getCountry(countryName);
     detailsStore.setCountry(country);
     detailsStore.changeState(false);
+  }
+
+  void changeFavoritesList(Country country) {
+    if (store.isFavorite == false) {
+      store.changeState(true);
+      store.setFavorite(country);
+      store.setListFavorites(favorite);
+    } else {
+      store.changeState(false);
+      store.removeFavorite(country);
+    }
   }
 }
--- lib/app/pages/favorites_page/favorites_store.dart
+++ lib/app/pages/favorites_page/favorites_store.dart
@@ -1,6 +1,36 @@
+import 'package:covid_data/app/models/country.dart';
 import 'package:mobx/mobx.dart';
 part 'favorites_store.g.dart';
 
 class FavoritesStore = _FavoritesStoreBase with _$FavoritesStore;
 
-abstract class _FavoritesStoreBase with Store {}
+abstract class _FavoritesStoreBase with Store {
+  @observable
+  bool isFavorite = false;
+
+  @observable
+  Country country = Country(
+      active: 0,
+      cases: 0,
+      continent: '',
+      country: '',
+      countryInfo: CountryInfo(flag: ''),
+      deaths: 0,
+      recovered: 0);
+
+  @observable
+  ObservableList favorites = <Country>[].asObservable();
+
+  @action
+  void changeState(bool isFavorite) => this.isFavorite = isFavorite;
+
+  @action
+  void setFavorite(Country country) => this.country = country;
+
+  @action
+  void removeFavorite(Country country) => this.country = country;
+
+  @action
+  void setListFavorites(List<Country> value) =>
+      favorites = value.asObservable();
+}
